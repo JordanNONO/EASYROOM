a2ea6bcf565fed4d8b90b9ddb3bf4cfc20758718
francis-tg <franc9885@gmail.com>
2024-03-22 11:55:58 +0000

    add auth page

---
+++ easyroom/lib/auth/Register.dart
@@ -0,0 +1,158 @@
+import 'package:easyroom/auth/Login.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    final TextEditingController contactController= TextEditingController();
+    return Scaffold(
+        body: SafeArea(
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Form(
+                      key: formKey,
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          children: <Widget>[
+                            const SizedBox(height: 50),
+                            const Center(
+                              child: Text("EASYROOM",style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w700
+                              ),),
+                            ),
+                            const SizedBox(height: 50),
+                            Container(
+                                padding: const EdgeInsets.all(5),
+                                child:const Center(  child: Text("Bienvenue",style: TextStyle(
+                                  fontSize: 30,
+                                ),),
+                                )
+                            ),
+                            const SizedBox(height: 20),
+                            TextFormField(
+                              controller: contactController,
+                              keyboardType: TextInputType.phone,
+                              decoration: const InputDecoration(
+                                hintText: 'Entrer votre numero de téléphone',
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal)
+                                ),
+                              ),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Numéro est requis';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 20),
+                            TextFormField(
+                              controller: emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                hintText: 'Votre email',
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal)
+                                ),
+
+                              ),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'L\'Email est requis';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 20),
+                            TextFormField(
+                              controller: passwordController,
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: true,
+                              obscuringCharacter: "*",
+                              decoration: const InputDecoration(
+                                hintText: 'Créer un mot de passe',
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal)
+                                ),
+
+                              ),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Un code secret est requis';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 20),
+                            TextFormField(
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: true,
+                              obscuringCharacter: "*",
+                              decoration: const InputDecoration(
+                                hintText: 'Repéter le mot de passe',
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal)
+                                ),
+
+                              ),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty || value != passwordController.text) {
+                                  return 'Un code secret est requis';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 20),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // Add your login logic here
+                                  // Typically, you would validate user input and authenticate the user.
+                                  if(formKey.currentState!.validate()){
+                                    //register(context);
+                                  }else {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        const SnackBar(
+                                            content: Text("Veuillez remplir les champs"))
+                                    );
+                                  }
+                                },
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(Colors.amber),
+                                  padding: MaterialStateProperty.all(const EdgeInsets.all(15)),
+
+                                ),
+
+                                child: const Text('Créer un compte',style: TextStyle(color: Colors.white),),
+                              ),
+
+
+                            ),
+                            const SizedBox(height: 20),
+                            Row(
+                              children: [
+                                const Text("Avez-vous déjà un compte?"),
+                                TextButton(onPressed: (){
+                                  Navigator.push(context, MaterialPageRoute(builder:(context)=>LoginPage()));
+                                }, child: const Text("connectez-vous"))
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                  ),
+                )
+              ],
+            )
+        )
+    );
+  }
+
+}
--- easyroom/lib/main.dart
+++ easyroom/lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:easyroom/auth/Login.dart';
 import 'package:easyroom/next_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main(){
   runApp(
-    MaterialApp(
-      home: MyApp(),
+     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+      ),
+      home: const LoginPage(),
     )
   );
 }
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -22,14 +28,14 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return  Scaffold(
            body: Container(
-             margin: EdgeInsets.only(top: 50.0, right: 20.0, left: 20.0),
-             child: Column(
+             margin: const EdgeInsets.only(top: 50.0, right: 20.0, left: 20.0),
+             child: SingleChildScrollView(child: Column(
                crossAxisAlignment: CrossAxisAlignment.start,
                children: <Widget>[
                  Row(
                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                    children: <Widget>[
-                     Text("Jordan NONO", style: TextStyle(
+                     const Text("Jordan NONO", style: TextStyle(
                        fontSize: 18.0,
                        fontWeight: FontWeight.bold,
                        color: Colors.black,
@@ -38,31 +44,31 @@ class _MyAppState extends State<MyApp> {
                      Container(
                        width: 50.0,
                        height: 50.0,
-                       decoration: BoxDecoration(
-                         shape: BoxShape.circle,
-                        color: Colors.blue,
-                        image: DecorationImage(image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2-6LFf4RA6G-oVz-3vYd9clN_jfnG1Ir0hEiAnm2gMg&s"), fit: BoxFit.cover)
+                       decoration: const BoxDecoration(
+                           shape: BoxShape.circle,
+                           color: Colors.blue,
+                           image: DecorationImage(image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2-6LFf4RA6G-oVz-3vYd9clN_jfnG1Ir0hEiAnm2gMg&s"), fit: BoxFit.cover)
                        ),
                      ),
                    ],
                  ),
-                 SizedBox(
+                 const SizedBox(
                    height: 20.0,
                  ),
-                 Text("Discover", style: TextStyle(
+                 const Text("Discover", style: TextStyle(
                    color: Colors.black,
                    fontWeight: FontWeight.bold,
                    fontSize: 30.0,
                  ),),
-                 SizedBox(
+                 const SizedBox(
                    height: 10.0,
                  ),
-                 Text("Suitable Home", style: TextStyle(
+                 const Text("Suitable Home", style: TextStyle(
                    color: Colors.black,
                    fontSize: 30.0,
 
                  ),),
-                 SizedBox(
+                 const SizedBox(
                    height: 20.0,
                  ),
 
@@ -71,15 +77,15 @@ class _MyAppState extends State<MyApp> {
                      Expanded(
                        child: Container(
                          decoration: BoxDecoration(
-                           color: Colors.blue.withOpacity(.5),
-                           borderRadius: BorderRadius.only(
-                             topLeft: Radius.circular(30.0),
-                             bottomLeft: Radius.circular(30.0),
-                             bottomRight: Radius.circular(30.0),
+                             color: Colors.blue.withOpacity(.5),
+                             borderRadius: const BorderRadius.only(
+                               topLeft: Radius.circular(30.0),
+                               bottomLeft: Radius.circular(30.0),
+                               bottomRight: Radius.circular(30.0),
 
-                           )
+                             )
                          ),
-                         child: TextField(
+                         child: const TextField(
                            decoration: InputDecoration(
                              prefixIcon: Icon(Icons.search),
                              hintText: "Find a good home",
@@ -88,35 +94,35 @@ class _MyAppState extends State<MyApp> {
                          ),
                        ),
                      ),
-                     SizedBox(
+                     const SizedBox(
                        width: 20.0,
                      ),
-                       Stack(
+                     Stack(
                        children: <Widget>[
-                     Icon(Icons.notifications_none),
-                     Positioned(
-                       top: 1,
-                       right: 1,
+                         const Icon(Icons.notifications_none),
+                         Positioned(
+                           top: 1,
+                           right: 1,
 
 
-                       child: Container(
-                         padding: EdgeInsets.all(2.0),
-                         child: Text("2", style: TextStyle(
-                           color: Colors.white,
-                           fontSize: 10.0
-                         ),),
-                         decoration: BoxDecoration(
-                           color: Colors.orange,
-                           shape: BoxShape.circle,
-                         ),
-                       ),
+                           child: Container(
+                             padding: const EdgeInsets.all(2.0),
+                             decoration: const BoxDecoration(
+                               color: Colors.orange,
+                               shape: BoxShape.circle,
+                             ),
+                             child: const Text("2", style: TextStyle(
+                                 color: Colors.white,
+                                 fontSize: 10.0
+                             ),),
+                           ),
+                         )
+                       ],
                      )
                    ],
-                 )
-               ],
-             ),
-                 SizedBox(height: 20.0,),
-                 Container(
+                 ),
+                 const SizedBox(height: 20.0,),
+                 SizedBox(
                    height: 400.0,
                    child: ListView(
                      scrollDirection: Axis.horizontal,
@@ -128,32 +134,32 @@ class _MyAppState extends State<MyApp> {
                      ],
                    ),
                  )
-                ],
-             ),
+               ],
+             ),)
            ),
       bottomNavigationBar: Container(
-        margin: EdgeInsets.only(bottom: 40.0),
+        margin: const EdgeInsets.only(bottom: 40.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            index == 1? _selectedWidget(Icon(Icons.home), "Home"): IconButton(icon:Icon(Icons.home), onPressed: (){
+            index == 1? _selectedWidget(const Icon(Icons.home), "Home"): IconButton(icon:const Icon(Icons.home), onPressed: (){
               setState(() {
                 index = 1;
               });
             },),
-            index == 2? _selectedWidget(Icon(Icons.bookmark_border), "Bookmaker") : IconButton(icon: Icon(Icons.bookmark_border),onPressed: (){
+            index == 2? _selectedWidget(const Icon(Icons.bookmark_border), "Bookmaker") : IconButton(icon: const Icon(Icons.bookmark_border),onPressed: (){
               setState(() {
                 index = 2;
               });
             },),
 
-            index == 3? _selectedWidget(Icon(Icons.message), "Messages") : IconButton(icon: Icon(Icons.message),onPressed: (){
+            index == 3? _selectedWidget(const Icon(Icons.message), "Messages") : IconButton(icon: const Icon(Icons.message),onPressed: (){
               setState(() {
                 index = 3;
               });
             },),
 
-            index == 4? _selectedWidget(Icon(Icons.person_outline), "Profile") : IconButton(icon: Icon(Icons.person_outline),onPressed: (){
+            index == 4? _selectedWidget(const Icon(Icons.person_outline), "Profile") : IconButton(icon: const Icon(Icons.person_outline),onPressed: (){
               setState(() {
                 index = 4;
               });
@@ -168,11 +174,11 @@ class _MyAppState extends State<MyApp> {
     return InkWell(
       onTap: (){
        Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-         return NextPage();
+         return const NextPage();
        }));
       },
       child: Container(
-        padding: EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
         width: 250.0,
         height: 400.0,
         child: Stack(
@@ -183,7 +189,7 @@ class _MyAppState extends State<MyApp> {
               
                 width: 350.0,
                 height: 400.0,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30.0),
                       bottomLeft: Radius.circular(30.0),
@@ -203,12 +209,12 @@ class _MyAppState extends State<MyApp> {
                 heroTag: null,
                 mini: true,
                 backgroundColor: Colors.orange,
-                child: Icon(Icons.chevron_right, color: Colors.white,), onPressed: (){
+                child: const Icon(Icons.chevron_right, color: Colors.white,), onPressed: (){
 
               },
               ),
             ),
-            Positioned(
+            const Positioned(
               bottom: 40,
               left: 20,
               child: Column(
@@ -239,9 +245,9 @@ class _MyAppState extends State<MyApp> {
 
   Widget _selectedWidget(Icon icon, String str){
     return Container(
-      padding: EdgeInsets.all(5.0),
+      padding: const EdgeInsets.all(5.0),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
+        borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(30.0),
           bottomLeft: Radius.circular(30.0),
           bottomRight: Radius.circular(30.0),
