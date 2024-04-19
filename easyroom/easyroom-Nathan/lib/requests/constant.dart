c743ac0ea73d330a92f483830ef220ded4af5ca2
francis-tg <franc9885@gmail.com>
2024-04-19 23:33:42 +0000

    fix chat error'

--- easyroom/easyroom-Nathan/lib/requests/constant.dart
+++ easyroom/easyroom-Nathan/lib/requests/constant.dart
@@ -1,3 +1,3 @@
-//String API_URL = "https://easyroom-2nq7.onrender.com";
-String API_URL = "http://192.168.5.253:4500";
+String API_URL = "https://easyroom-2nq7.onrender.com";
+//String API_URL = "http://192.168.5.253:4500";
 String BASE_URL = "$API_URL/api/v1";
